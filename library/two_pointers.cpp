a5f70e47e914c11cd9e781b7fa57eaee05260a69
woxjro <yoshi_nn_ihsoy@icloud.com>
2020-05-31 09:40:01 +0900

    しゃくとり法を追加

---
+++ library/two_pointers.cpp
@@ -0,0 +1,35 @@
+#include <bits/stdc++.h>
+#define int long long
+using namespace std;
+
+void solve()
+{
+
+    cin.tie(0);
+    ios::sync_with_stdio(false);
+
+    int n;
+    cin >> n;
+    int a[n];
+    for (int i = 0; i < n; i++)
+        cin >> a[i];
+    int ans = 0;
+
+    int r = 0;
+    for (int l = 0; l < n; l++)
+    {
+        if (l == r)
+            r++;
+        for (; r < n && a[r - 1] < a[r]; r++)
+            ;
+        ans += r - l;
+    }
+
+    cout << ans << endl;
+}
+
+signed main()
+{
+    solve();
+    return 0;
+}
